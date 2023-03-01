258b912b242fff91ea894c7a956db5df3a113c21
bellettichloe <123496194+bellettichloe@users.noreply.github.com>
2023-03-01 09:24:57 -0500

    Update CanvasAPI_get_user_info.py
    Lists all canvas courses four user

--- CanvasAPI_get_user_info.py
+++ CanvasAPI_get_user_info.py
@@ -1,7 +1,6 @@
 # imports
 from canvasapi import Canvas
 
-import datetime
 # API URL
 API_URL = 'https://uc.instructure.com'
 # User input for API Key
@@ -9,10 +8,8 @@ API_KEY = '1109~8XCwn02uDMRouJecaJILOnr17FPD16u67HxaPHYS6yhh4XVeJmF1y0mOrQOHy08f
 
 canvas = Canvas(API_URL, API_KEY)
 user = canvas.get_current_user()
+
 # Get the default calendar for the user
-print(canvas.get_current_user())
-todo_list = canvas.get_todo_items()
-print(todo_list)
 courses = user.get_courses()
 for course in courses:
     print(course)
