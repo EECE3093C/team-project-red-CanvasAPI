31fe99b416af07dcb8a20d38050f7c5e7e03445d
bellettichloe <123496194+bellettichloe@users.noreply.github.com>
2023-03-06 18:05:19 -0500

    commit

--- get_assignment_list.py
+++ get_assignment_list.py
@@ -2,6 +2,7 @@
 from canvasapi import Canvas
 from canvasapi.canvas_object import CanvasObject
 from canvasapi.exceptions import CanvasException, RequiredFieldMissing
+from canvasapi.calendar_event import CalendarEvent
 from canvasapi.grade_change_log import GradeChangeEvent
 from canvasapi.paginated_list import PaginatedList
 from canvasapi.peer_review import PeerReview
@@ -11,16 +12,19 @@ from canvasapi.upload import FileOrPathLike, Uploader
 from canvasapi.user import User, UserDisplay
 from canvasapi.util import combine_kwargs, obj_or_id
 
-
 # API URL
 API_URL = 'https://uc.instructure.com'
+
 # User input for API Key
 API_KEY = '1109~8XCwn02uDMRouJecaJILOnr17FPD16u67HxaPHYS6yhh4XVeJmF1y0mOrQOHy08f' # chloe's for testing
+''' API_KEY = input("Enter your API key for canvas: ")'''
 
 # Initializing canvas object
 canvas = Canvas(API_URL, API_KEY)
+
 # Initializing canvas user
 user = canvas.get_current_user()
+print("Logged in as: ", user.name)
 
 # Get a paginated list of courses
 course_list = user.get_courses()
@@ -45,9 +49,13 @@ for id in current_courses:
         if assignment.due_at:
             x = [assignment.due_at, course.name, assignment.name]
             current_assignments.append(x)
-sorted_assignments = sorted(current_assignments, key = lambda x: x[0])
+sorted_assignments = sorted(current_assignments, key = lambda x: x[0]) # This is too slow
 
 ## to view assignments 
 """ for assignment in sorted_assignments:
     print(assignment[0], assignment[1], assignment[2]) """
 
+events = canvas.get_calendar_events()
+print('x')
+for event in events:
+    print('test')   
